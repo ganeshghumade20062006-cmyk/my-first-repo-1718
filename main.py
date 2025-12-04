4754bfae2ebe46b833e70898a7e5e25aee2ee5a1
Ganesh Ghumade <ganeshghumade20062006@gmail.com>
2025-12-04 18:22:25 +0530

    Daily update

---
+++ main.py
@@ -0,0 +1,38 @@
+# DAY 1 OF CODING
+
+
+
+
+
+print("hello world")
+print("ganesh ghumade moreshwar")
+print(458*455)
+print(455+4588)
+print("ganesh ghumde \nmoreshwar \"ghumade\"")
+# OUTPUT= ganesh ghumde 
+# moreshwar "ghumade"
+print("yavatmal", "chanddrapur", sep="~")
+# OUTPIT= yavatmal~chanddrapur
+print("yavatmal from ganesh ghumade\nand chandrapur is a good city")
+# OUTPUT= yavatmal from ganesh ghumade
+# and chandrapur is a good city
+print("ganesh  ghumade")
+a="ganesh ghumade"
+b="ghumade ganesh"
+c=5656
+d="vaishali ghuamde"
+print("the type of a is", type(a))
+print("the type of b is", type(b))
+print("the type of c is", type(c))
+print("thr type of d is ", type(d))
+# OUTPUT= he type of a is <class 'str'>
+# the type of b is <class 'str'>
+# the type of c is <class 'int'>
+# # thr type of d is  <class 'str'>
+print("today is holiday")
+1
+2
+3
+4
+5
+# DAY 2 OF CODING....
